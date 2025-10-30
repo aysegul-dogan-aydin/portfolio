1e3d82dcb8fd326090797d64e1e2a47096ef4716
Selim Dilşad Ercan <selimdilsadercan@gmail.com>
2025-10-30 17:24:42 +0300

    hmm

--- data/settings.ts
+++ data/settings.ts
@@ -1,7 +1,7 @@
 const settings = {
-  signature: "https://bmxnokmxpfhnlsughdtt.supabase.co/storage/v1/object/public/photos/assets/unnamed.png",
+  signature: "https://polite-cuttlefish-271.convex.cloud/api/storage/15ab18a7-81aa-4abb-8e07-2c3c7ae97099",
   name: "Aysegul Aydin",
-  statement_image: "https://bmxnokmxpfhnlsughdtt.supabase.co/storage/v1/object/public/photos/settings/1727375677668000.jpg",
+  statement_image: "https://polite-cuttlefish-271.convex.cloud/api/storage/4ab3b8e2-cd4f-40fc-8888-25d763a459cb",
   statement_description:
     "Turco, nato in Germania nel 1975, vive tra Istanbul e Venezia. Artista transdisciplinare, ingegnere ambientale, fotografo, performer, life coach. Hapartecipato a diversi workshop tenuti in Italia, Germania e Turchia La sua ricerca si costituisce di diversi medium sia concettuali che materiali.Ama giocare con le lingue in cerca di testimonia. Crea i suoi lavori in una maniera che RE-/DE-construisce una legame nel temporale essistenza traspettattore-opera-e-artista, e transforma l'idea in un'identità universale. L' approccio significativo del suo opera è minimale e puro, che comportauna dimensione di densità. Le sue opere più recenti sono focalizzate sugli approcci matematici."
 };
